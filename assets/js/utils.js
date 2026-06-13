4c3b7ca5631078d3285ea38bbd747dcbfe0f09b0
yelderiny <yasmin.elderiny@gmail.com>
2026-06-13 19:46:58 +0300

    Add full Quran Uthmani text locally, remove API dependency
    Store all 6236 ayahs (Tanzil Uthmani edition) in assets/data/quran-uthmani.json
    as a 2D array [surah-1][ayah-1]. Add getAyah(s,a) and initQuranText() to utils.js.
    Update mutashabihat-awail to load from local file instead of alquran.cloud API.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,3 +1,18 @@
 function toAr(n) {
   return String(n).replace(/[0-9]/g, d => '٠١٢٣٤٥٦٧٨٩'[d]);
 }
+
+// Full Quran text (Uthmani script). Loaded once via initQuranText().
+// Structure: _qt[surah-1][ayah-1] = raw Uthmani text string.
+var _qt = null;
+
+function initQuranText(data) {
+  _qt = data;
+}
+
+// Returns the Uthmani text of the given surah/ayah, or '' if not yet loaded.
+function getAyah(s, a) {
+  if (!_qt) return '';
+  var surah = _qt[s - 1];
+  return surah ? surah[a - 1] || '' : '';
+}
